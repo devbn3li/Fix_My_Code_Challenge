cd6ecc8efdad91f0ef91d44ccb210b7063b8c274
Mohamed Ali <76428295+devbn3li@users.noreply.github.com>
2023-12-19 22:23:57 +0200

    Create user.py

---
+++ 0x01-challenge/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" 
+User class
+"""
+
+class User:
+    def __init__(self):
+        self.__email = None
+
+    @property
+    def email(self):
+        """Getter for email"""
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        """Setter for email"""
+        if not isinstance(value, str):
+            raise TypeError("email must be a string")
+        self.__email = value
+
+if __name__ == "__main__":
+    u = User()
+    u.email = "john@snow.com"
+    print(u.email)
+  
